7134438bc44009db7d293ff6a4a7b37283f8e15e
Egbewatt kokou <ksupro1@gmail.com>
2020-01-15 09:06:19 +0100

    --fix

--- server/domains/models/categoryModel.js
+++ server/domains/models/categoryModel.js
@@ -7,6 +7,7 @@
  */
 import winstonLogger from '../../Infrastructure/utils/winstonLogger'
 import mongoose from '../../Infrastructure/plugins/mongooseCon'
+require('mongoose-type-url')
 
 const TSchema = mongoose.Schema
 
